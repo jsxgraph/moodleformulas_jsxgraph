181a7de6f4d411a6ad0c926ce47285cc91ebc7b6
alfred <alfred.wassermann@uni-bayreuth.de>
2020-06-19 13:27:56 +0200

    Work on removing jQuery

--- src/jsxquestion.js
+++ src/jsxquestion.js
@@ -20,7 +20,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
 */
-
+/*
+<!--<script type="text/javascript" 
+src="https://jsxgraph.uni-bayreuth.de/~alfred/moodleformulas_jsxgraph/src/jsxquestion.js"></script>
+-->
+*/
 "use strict";
 /**
  *
@@ -29,30 +33,64 @@
  * @param {Boolean} debug  Debug flag. If false the input elements of the formulas question are hidden.
  */
 var JSXQuestion = function (elID, jsxCode, debug) {
-    var that = this;
+    var that = this,
+        topEl;
 
     /**
      * HTML element containing the board
+     * 
+     * @type {String}
      */
     this.elm = document.getElementById(elID);
 
     // Get the first ancestor of the board having class ".formulaspart" (should be a div)
-    // and get all input elements there in.
-    that.inputs = $(that.elm).closest(".formulaspart").find("input");
+    topEl = this.elm.closest('.formulaspart');
+
+    /**
+     * Stores the input tags from the formulas question.
+     * 
+     * @type {Array}
+     */
+    // this.inputs = $(this.elm).closest(".formulaspart").find("input");
+    this.inputs = topEl.querySelectorAll('input');
+
     // Hide the outcome div and the input elements
-    $(that.elm).closest(".formulaspart").children("formulaspartoutcome").hide();
+    // $(this.elm).closest(".formulaspart").children("formulaspartoutcome").hide();
+    topEl.querySelectorAll('.formulaspartoutcome').forEach(el => { el.style.display = 'none'; });
     if (debug !== true) {
-        that.inputs.hide();
+        // this.inputs.hide();
+        this.inputs.forEach(el => { el.style.display = 'none'; });
     }
-    that.solved = $(that.inputs[0]).prop('readonly');
-    // Function to fill formulas input form of index idx with a value.
-    that.set = function (idx, val) {
-        if (!that.solved && that.inputs && that.inputs[idx]) {
+
+    /**
+     * Indicator if the question has been solved.
+     * 
+     * @type {Boolean}
+     */
+    if (this.inputs && this.inputs[0]) {
+        this.isSolved = this.inputs[0].readOnly;
+    } else {
+        this.isSolved = false;
+    }
+
+    /**
+     * Fill input element of index idx of the formulas question with value.
+     * 
+     * @param {Number} idx Index of the input element, starting at 0.
+     * @param {Number} val Number to be set.
+     */
+    this.set = function (idx, val) {
+        if (!that.isSolved && that.inputs && that.inputs[idx]) {
             that.inputs[idx].value = Math.round(val * 100) / 100;
         }
     };
-    // Function to get the content of formulas input form of index idx.
-    that.get = function (idx) {
+
+    /**
+     * Get the content of input element of index idx of the formulas question.
+     * 
+     * @param {Number} idx Index of the input form, starting at 0.
+     */
+    this.get = function (idx) {
         if (that.inputs && that.inputs[idx]) {
             var n = parseFloat(that.inputs[idx].value);
             if (isNaN(n)) {
@@ -62,10 +100,17 @@ var JSXQuestion = function (elID, jsxCode, debug) {
         }
         return null;
     };
+
+    this.brd = null;
+    this.board = null;
+
     // Execute the JSXGraph / JavaScript code
-    that.brd = null;
-    jsxCode(that);
-    that.reload = function () {
+    jsxCode(this);
+
+    /**
+     * Reload the construction
+     */
+    this.reload = function () {
         jsxCode(that);
     };
 };
